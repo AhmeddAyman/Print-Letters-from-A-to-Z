50d73582e7f63df44538426ab2afcf071bd72ff8
AhmeddAyman <ahmedaiman12312312@gmail.com>
2024-11-28 22:31:43 +0200

    Add project files.

---
+++ Print Letters from A to Z/Print Letters from A to Z.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+// #Course_4_Solution_46
+// Print Letters from A to Z.
+
+void PrintLettersAtoZ()
+{
+	for (int i = 65; i <= 90; i++)
+	{
+		cout << char(i) << endl;
+	}
+}
+
+
+int main()
+{
+	PrintLettersAtoZ();
+		return 0;
+}
